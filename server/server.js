582a3bd4951be3df31529e142912d06c991c5c67
Ahmad <leonn_8m@hotmail.com>
2017-08-11 18:30:42 +0300

    modifing environment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const {mongoose} = require('./../db/db');
-const {Category} = require('./../db/models/category');
+// const {mongoose} = require('./../db/db');
+// const {Category} = require('./../db/models/category');
 
 const port = process.env.PORT || 3000;
 const app = express();
